5b121919ffd11986378fc0f7ca63037aa6b512bc
Bart Kastermans <kasterma@kasterma.net>
2022-08-24 14:10:13 +0200

    WIP

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd /Users/kasterma
+
+git clone git@github.com:kasterma/dotfiles.git dotfiles-gh  # needs ssh keys setup
+
+ln -s dotfiles-gh/dotbash_profile .bash_profile
+ln -s dotfiles-gh/dotbashrc .bashrc
+ln -s dotfiles/dotgitconfig .gitconfig
+ln -s dotfiles/dotgitignore_global .gitignore_global
+ln -s dotfiles-gh/dottmux.conf .tmux.conf
+ln -s dotfiles-gh/dotemacs .emacs
+
+brew install pyenv bash-completions
